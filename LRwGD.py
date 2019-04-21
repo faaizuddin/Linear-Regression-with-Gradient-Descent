d530803fab963ed619da980fcc7221937f2019a6
Faaiz Qadri <49518784+faaizuddin@users.noreply.github.com>
2019-04-21 14:10:51 +0200

    Update LRwGD.py

--- LRwGD.py
+++ LRwGD.py
@@ -15,13 +15,6 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
-#Ex 1
-
-#creating column names for airq402 data
-nc=["City1","City2","Average Fair","Distance","Avg Weekly Passengers","Market Leading Airline","Market Share","Average Fair","Low Price Airline","Market Share","Price"] #header names
-
-airq402=pd.read_csv("http://www.stat.ufl.edu/~winner/data/airq402.dat", names=nc, sep="\s+", header=None)
-print(airq402)
 
 #creating column names for wine quality data
 nc1=["fixed acidity","volatile acidity","citric acid","residual sugar","chlorides","free sulfur dioxide","total sulfur dioxide","density","pH","sulphates","alcohol","quality"]
@@ -32,36 +25,7 @@ print(wqred)
 wqwhite=pd.read_csv("winequality-white.csv",sep=';', header=None, names=nc1)
 print(wqwhite)
 
-dframe=pd.DataFrame(airq402)
-
-# this function is converting categorical values into numerical values
-def handle_non_numeric_data(dframes):
-    columns=dframes.columns.values
-    
-    for column in columns:
-        text_digit_values={}
-        def convert_to_int(val):
-            return text_digit_values[val]
-        
-        if dframes[column].dtype != np.int64 and dframes[column].dtype != np.float64:
-            column_content=dframes[column].values.tolist()
-            unique_elements=set(column_content)
-            x=0
-            for unique in unique_elements:
-                if unique not in text_digit_values:
-                    text_digit_values[unique]=x
-                    x+=1
-            dframes[column]=list(map(convert_to_int,dframes[column]))
-            
-    return dframes
-
-dframe=handle_non_numeric_data(dframe)
-print(dframe.head())
-
-
-# finding rows with null values in all three datasets
-null_data = airq402[airq402.isnull().any(axis=1)]
-print(null_data)
+# finding rows with null values in both the datasets
 
 null_data1 = wqred[wqred.isnull().any(axis=1)]
 print(null_data1)
@@ -69,11 +33,7 @@ print(null_data1)
 null_data2 = wqwhite[wqwhite.isnull().any(axis=1)]
 print(null_data2)
 
-#splitting all the three data sets
-splitdata=np.random.rand(len(dframe))<0.8
-aqtrain=dframe[splitdata]
-aqtest=dframe[~splitdata]
-print(aqtrain,aqtest)
+#splitting both the data sets
 
 splitdata=np.random.rand(len(wqred))<0.8
 wqredtrain=wqred[splitdata]
@@ -86,11 +46,7 @@ wqwhitetest=wqwhite[~splitdata]
 print(wqwhitetrain,wqwhitetest)
 
 
-
-# =============================================================================
-#Ex 2
-# =============================================================================
-
+# Implementing the Gradient Descent Algoithm
 def gradient_descent(Beta,XB,Y,Alpha,iteration,z):
      XT= np.transpose(X)
      la=[] 
@@ -102,6 +58,7 @@ def gradient_descent(Beta,XB,Y,Alpha,iteration,z):
     
      for i in range(0,iteration):  
          
+       
          #∂f/∂βˆ(βˆ) = −2(XTy − XT Xβˆ)= −2XT(y − Xβˆ)
          XB=np.dot(X,Beta)
          H=np.subtract(Y,XB)
@@ -119,7 +76,9 @@ def gradient_descent(Beta,XB,Y,Alpha,iteration,z):
      plt.plot(la)
      plt.show()
 
-        
+# The values of test data is used in the function defined.
+# Outside the block, we insert bias column and take the integer values of the trained dataset.     
+
 def lf(X,Y,Beta):   
      predY=np.dot(X,Beta) # Predicted value of Y
      loss=np.subtract(Y,predY)
@@ -135,8 +94,8 @@ print(X)
 Y=wqredtrain.iloc[:,12]
 XB= np.column_stack((bias, X))
 iteration=1000
-Alpha=0.0000002
-Beta=np.zeros(len(wqredtrain.iloc[0:12]),float)
+Alpha=0.0000002 # The values of step length (learning rate) are kept low as keeping it higher was not producing the correct graph.
+Beta=np.zeros(len(wqredtrain.iloc[0:12]),float) # adds a column of 0s
 gradient_descent(Beta,XB,Y,Alpha,iteration,wqredtest)
 
 # calculating gradient descent for white wine
@@ -151,15 +110,4 @@ Alpha=0.00000001
 Beta=np.zeros(len(wqwhitetrain.iloc[0:12]),float)
 gradient_descent(Beta,XB,Y,Alpha,iteration,wqwhitetest)
 
-# =============================================================================
-# bias= np.ones((len(aqtrain), 1))
-# aqtrain.insert(0,'bias',bias)
-# X=aqtrain[:,0:12]
-# Y=aqtrain[:,12]
-# XB= np.column_stack((bias, X))
-# iteration=1000
-# Alpha=0.00000001
-# Beta=np.zeros(len(aqtrain[0:12]),float)
-# gradient_descent(Beta,XB,Y,Alpha,iteration,aqtest)
-# =============================================================================
-
+# The same procedure is used for both the wine quality data set and is implemented by the ‘gradient_descent’ function.
